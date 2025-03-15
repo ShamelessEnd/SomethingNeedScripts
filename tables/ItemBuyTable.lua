195957af1fba644e769d1f32be5f1832dcdaead5
ShamelessEnd <shamelessend@hotmail.com>
2025-03-15 15:26:50 +1100

    wheel

---
+++ tables/ItemBuyTable.lua
@@ -0,0 +1,9 @@
+WheelBuyTable = {
+    --   id, count, price
+    { 12228,   900,  1000 }, -- Deep-green Cluster
+    { 12606,   300,  4000 }, -- Astral Oil
+    { 12936,   900,  4500 }, -- Duskborne Aethersand
+    {  5066,   900,  1800 }, -- Electrum Ingot
+    { 12531,   900,   500 }, -- Mythrite Sand
+    { 10099,   900,   300 }, -- Unaspected Crystal
+}
