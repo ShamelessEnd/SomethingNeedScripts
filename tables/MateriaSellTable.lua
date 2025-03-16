4b2f01105db8ee58699db028cfaaad42854f62cb
ShamelessEnd <shamelessend@hotmail.com>
2025-03-16 20:22:49 +1100

    materia sell

---
+++ tables/MateriaSellTable.lua
@@ -0,0 +1,14 @@
+MateriaSellTable = {
+    [1] = {
+      config = { exclude=false, unlist=false, entrust=false },
+      sell_table = {
+        --   id, price floor, force list, stack size, max listings, min keep    -- name
+        { 41759,        9999,       true,         99,            4,     3999 }, -- Crit 11
+        { 41772,       29999,       true,         20,            2,      400 }, -- Crit 12
+        { 41758,       14999,       true,         99,            5,     4999 }, -- Dhit 11
+        { 41771,       29999,       true,         20,            3,      400 }, -- Dhit 12
+        { 41760,        9999,       true,         99,            4,     2999 }, -- Det  11
+        { 41773,       24999,       true,         20,            2,      250 }, -- Det  12
+      }
+    },
+}
