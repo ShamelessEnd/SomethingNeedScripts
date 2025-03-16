8f29c38bb90a164a62c32f6a55b5dd0715bc9c62
ShamelessEnd <shamelessend@hotmail.com>
2025-03-16 20:23:57 +1100

    hunt log fns

--- src/QuestHelper.lua
+++ src/QuestHelper.lua
@@ -221,3 +221,319 @@ function UnlockTeleport()
   yield("/wait 0.2")
   yield("/interact")
 end
+
+local function enterAetherialFlow()
+  NavToObject("Aetherial Flow", 3, false, 10)
+  InteractWith("Aetherial Flow", "SelectYesno", 5)
+  Callback("SelectYesno", true, 0)
+  yield("/wait 3")
+  WaitUntil(function () return IsPlayerAvailable() and NavIsReady() and IsInZone(1245) end)
+end
+
+function FollowPartyLeader()
+  while true do
+    WaitForNavReady()
+    if IsAddonReady("SelectYesno") and StringStartsWith(GetNodeText("SelectYesno", 15), "Accept Teleport") then
+      Callback("SelectYesno", true, 0)
+    elseif IsAddonReady("SelectYesno") and StringStartsWith(GetNodeText("SelectYesno", 15), "Accept Raise") then
+      Callback("SelectYesno", true, 0)
+    elseif IsAddonReady("ContentsFinderConfirm") then
+      Callback("ContentsFinderConfirm", true, 8)
+    elseif not HasTarget() then
+      local lead_name = GetPartyMemberName(GetPartyLeadIndex())
+      if not TerritorySupportsMounting() and StringIsEmpty(lead_name) then
+        yield("/autofollow")
+        LeaveDuty()
+      else
+        yield("/target "..GetPartyMemberName(GetPartyLeadIndex()))
+      end
+    elseif IsTargetMounted() and GetDistanceToTarget() < 5 then
+      if not GetCharacterCondition(10) then yield("/ridepillion <t>") end
+    elseif not TerritorySupportsMounting() then
+      if GetDistanceToObject("Aetherial Flow") < 30 then
+        enterAetherialFlow()
+      else
+        NavToTarget(GetTargetName(), 1, false, 10)
+      end
+    elseif GetDistanceToTarget() > 5 then
+      yield("/follow")
+      yield("/gaction jump")
+    end
+    if TerritorySupportsMounting() then
+      yield("/wait 0.5")
+    else
+      yield("/wait 0.1")
+    end
+  end
+end
+
+function MountAndWaitPillion()
+  while not IsMounted() do
+    yield("/mount \"Draught Chocobo\"")
+    yield("/wait 2")
+  end
+
+  local function isPartyMounted()
+    for i = 0,7 do
+      local name = GetPartyMemberName(i)
+      if not StringIsEmpty(name) and (GetDistanceToPartyMember(i) > 0.1 or GetPartyMemberHPP(i) < 20) then
+        return false
+      end
+    end
+    return true
+  end
+  WaitUntil(function () return isPartyMounted() end)
+end
+
+function IsPartyInCombat()
+  if IsInCombat() then return true end
+  local own_name = GetCharacterName()
+  for i = 0,7 do
+    local name = GetPartyMemberName(i)
+    if not StringIsEmpty(name) and name ~= own_name then
+      yield("/target "..name)
+      if GetTargetName() == name then
+        local inCombat = IsTargetInCombat()
+        ClearTarget()
+        if inCombat then return true end
+      end
+    end
+  end
+  return false
+end
+
+function KillMobs(targets)
+  local function needKill()
+    for _, count in pairs(targets) do
+      if count > 0 then return true end
+    end
+    return false
+  end
+
+  local partyCombat = IsPartyInCombat()
+  while needKill() or partyCombat do
+    if partyCombat then
+      TargetClosestEnemy()
+      yield("/wait 0.2")
+    else
+      for name, count in pairs(targets) do
+        if count > 0 then
+          yield("/target "..name)
+          yield("/wait 0.2")
+          if HasTarget() and GetDistanceToTarget() < 25 then
+            break
+          end
+        end
+      end
+    end
+
+    if HasTarget() and GetDistanceToTarget() < 25 then
+      local name = GetTargetName()
+      local killTimeout = 10
+      local fateID = GetTargetFateID()
+      local isFateMob = fateID and fateID > 0
+      if isFateMob then
+        killTimeout = 30
+        yield("/levelsync on")
+        yield("/wait 0.1")
+      end
+      yield("/wrath auto on")
+      yield("/wait 0.1")
+      if WaitWhile(HasTarget, killTimeout) and targets[name] then
+        targets[name] = targets[name] - 1
+      end
+      yield("/wrath auto off")
+      yield("/wait 0.1")
+      if isFateMob then
+        yield("/levelsync off")
+        yield("/wait 0.5")
+      end
+
+      if (GetHP() / GetMaxHP()) < 0.5 then
+        yield("/target <me>")
+        yield("/wrath auto on")
+        yield("/wait 1")
+        WaitUntil(function () return (GetHP() / GetMaxHP()) > 0.2 end)
+        yield("/wrath auto off")
+        yield("/wait 0.1")
+        WaitWhile(IsCasting)
+        ClearTarget()
+      end
+    else
+      yield("/wait 1")
+      ClearTarget()
+    end
+
+    partyCombat = IsPartyInCombat()
+  end
+end
+
+local function goToMobLocation(zone, aetheryte, x, y, z)
+  KillMobs({})
+  ResPartyMembers()
+  if not IsInZone(zone) then
+    TeleportToAetheryte(aetheryte)
+  end
+  if TerritorySupportsMounting() then
+    MountAndWaitPillion()
+  end
+  NavToPoint(x, y, z, 1, TerritorySupportsMounting(), 300)
+  yield("/wait 0.2")
+  while IsMounted() do
+    yield("/mount")
+    yield("/wait 0.5")
+  end
+end
+
+function ResPartyMembers()
+  local resd = false
+  for i = 0,7 do
+    local name = GetPartyMemberName(i)
+    if not StringIsEmpty(name) and GetPartyMemberHP(i) <= 0.1 then
+      yield("/target "..name)
+      yield("/wait 0.2")
+      yield("/ac Swiftcast <wait.1>")
+      yield("/ac Raise <t> <wait.9>")
+      resd = true
+    end
+  end
+  return resd
+end
+
+function GoToKillMobs(zone, aetheryte, x, y, z, name, count)
+  goToMobLocation(zone, aetheryte, x, y, z)
+  local targets = {}
+  targets[name] = count
+  KillMobs(targets)
+end
+
+function GoToKillMobsMulti(zone, aetheryte, x, y, z, targets)
+  goToMobLocation(zone, aetheryte, x, y, z)
+  KillMobs(targets)
+end
+
+function HuntingLogPrereqs()
+  local function mountChocobo() while not IsMounted() do yield("/mount \"Company Chocobo\"") WaitUntil(IsMounted, 3) end end
+  local function waitZoneTransfer(zone) WaitUntil(function () return IsPlayerAvailable() and NavIsReady() and IsInZone(zone) end) end
+  local function unlockAetheryte(zone, x, y, z)
+    waitZoneTransfer(zone)
+    mountChocobo()
+    NavToPoint(x, y, z, 7, false, 120)
+    yield("/target aetheryte")
+    yield("/wait 0.2")
+    yield("/interact")
+    yield("/wait 5")
+    if IsAddonVisible("SelectString") then
+      Callback("SelectString", true, -1)
+    end
+    WaitUntil(IsPlayerAvailable)
+  end
+
+  -- Halatali
+  yield("/at n")
+  WaitUntil(IsPlayerAvailable)
+  yield("/item "..GetItemName(30362))
+  waitZoneTransfer(140)
+  mountChocobo()
+  NavToPoint(-472.5, 23, -355, 3, false, 60)
+  yield("/at y")
+  yield("/target Nedrick Ironheart")
+  yield("/wait 0.2")
+  yield("/interact")
+  if AwaitAddonReady("SelectIconString", 5) then
+    Callback("SelectIconString", true, 0)
+    yield("/wait 2")
+  end
+  if IsAddonVisible("JournalAccept") then
+    Callback("JournalAccept", true, -1)
+  end
+  WaitUntil(IsPlayerAvailable)
+  yield("/at n")
+  yield("/wait 1")
+  TeleportToAetheryte(18)
+  waitZoneTransfer(145)
+  mountChocobo()
+  NavToPoint(-331, -22.5, 434.9, 3, false, 120)
+  yield("/at y")
+  yield("/wait 3")
+  WaitForNavReady()
+
+  -- Fallgourd
+  TeleportToGridania()
+  yield("/li North Shroud")
+  unlockAetheryte(154, -41.6, -38.6, 233.8)
+
+  -- Wineport
+  TeleportToAetheryte(52)
+  waitZoneTransfer(134)
+  mountChocobo()
+  NavToPoint(-164, 35, -725, 5, false, 120)
+  PathfindAndMoveTo(-162, 36, -737, false)
+  unlockAetheryte(137, -18.4, 72.7, 3.8)
+
+  -- Bronze Lake
+  NavToPoint(79, 80, -114, 5, false, 120)
+  PathfindAndMoveTo(77, 80, -121, false)
+  unlockAetheryte(139, 437.4, 5.5, 94.6)
+end
+
+function DoHuntingLogCarried()
+  HuntingLogPrereqs()
+  TeleportToAetheryte(18)
+  WaitUntil(function () return IsPlayerAvailable() and NavIsReady() and IsInZone(145) end)
+  FollowPartyLeader()
+end
+
+function DoHuntingLogCarry()
+  -- East Than
+  GoToKillMobs(145, 18, -49, -27, 330, "Amalj'aa Javelinier", 1)
+  GoToKillMobs(145, 18, -91, -29, 285, "Amalj'aa Javelinier", 2)
+  GoToKillMobsMulti(145, 18, 202, 13, -42, { ["Amalj'aa Ranger"] = 1, ["Amalj'aa Bruiser"] = 1 })
+  GoToKillMobsMulti(145, 18, 172, 15, -76, { ["Amalj'aa Ranger"] = 1, ["Amalj'aa Bruiser"] = 1 })
+  GoToKillMobsMulti(145, 18, 157, 12, -44, { ["Amalj'aa Ranger"] = 1, ["Amalj'aa Bruiser"] = 1 })
+
+  -- East Shroud
+  GoToKillMobs(152, 4, -138, 15, 3, "Sylvan Scream", 1)
+  GoToKillMobs(152, 4, -96, 20, 8, "Sylvan Scream", 2)
+
+  -- North Shroud
+  GoToKillMobs(154, 7, 72.9, -39.1, 340.2, "Ixali Deftalon", 3)
+
+  -- CCH
+  ResPartyMembers()
+  MountAndWaitPillion()
+  PathfindAndMoveTo(-368, 10, 182, true)
+  WaitUntil(function () return IsInZone(155) end)
+  GoToKillMobs(155, nil, 480, 233, 318, "Ixali Fearcaller", 1)
+  TeleportToGridania()
+
+  -- Halatali
+  OpenRegularDuty(7)
+  AwaitAddonReady("ContentsFinder")
+  SetDFUnrestricted(true)
+  while not IsNodeVisible("ContentsFinder", 1, 62, 64, 65) do
+    Callback("ContentsFinder", true, 3, 4)
+    yield("/wait 1")
+  end
+  Callback("ContentsFinder", true, 12, 0)
+  AwaitAddonReady("ContentsFinderConfirm")
+  Callback("ContentsFinderConfirm", true, 8)
+  WaitUntil(function () return IsPlayerAvailable() and NavIsReady() and IsInZone(1245) end)
+  WaitUntil(function () return IsAddonVisible("_Image") end)
+  yield("/wait 1")
+  RebuildNavMesh()
+  GoToKillMobs(1245, nil, 200, 6, -36, "Heckler Imp", 1)
+  GoToKillMobs(1245, nil, 153, -2, -9, "Heckler Imp", 2)
+  GoToKillMobs(1245, nil, 111, -4, 50, "Heckler Imp", 2)
+  GoToKillMobs(1245, nil, 29, 1, 125, "Firemane", 1)
+
+  enterAetherialFlow()
+  GoToKillMobs(1245, nil, 86, -9, -87, "Scythe Mantis", 2)
+  GoToKillMobsMulti(1245, nil, 46, -11, -93, { ["Coliseum Python"] = 1, ["Scythe Mantis"] = 2 })
+  GoToKillMobs(1245, nil, -2, -3, -115, "Scythe Mantis", 3)
+  GoToKillMobsMulti(1245, nil, 4, -11, -188, { ["Coliseum Python"] = 1, ["Scythe Mantis"] = 2 })
+  GoToKillMobsMulti(1245, nil, -8, -11, -156, { ["Coliseum Python"] = 1, ["Scythe Mantis"] = 2 })
+  GoToKillMobsMulti(1245, nil, -83, -11, -100, { ["Coliseum Python"] = 1, ["Scythe Mantis"] = 2 })
+  LeaveDuty()
+
+end
