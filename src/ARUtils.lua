5904514f15a9db420aeabc83c28353a438078765
ShamelessEnd <shamelessend@hotmail.com>
2025-03-29 10:41:29 +1100

    drop name check

--- src/ARUtils.lua
+++ src/ARUtils.lua
@@ -2,9 +2,8 @@ require "Logging"
 
 function GetARCharacterData()
   Logging.Debug("fetching AR character data")
-  local char = GetCharacterName(true)
   local data = ARGetCharacterData(GetPlayerContentId())
-  if data ~= nil and type(data) == "userdata" and data.Name.."@"..data.World == char then
+  if data ~= nil and type(data) == "userdata" then
     return data
   end
   return nil
