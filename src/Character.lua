50d62af64ba0a56468ab6d8ed5c218016afd8efb
ShamelessEnd <shamelessend@hotmail.com>
2025-04-13 00:58:32 +1000

    add character

---
+++ src/Character.lua
@@ -0,0 +1,22 @@
+require "Callback"
+require "Logging"
+require "UINav"
+require "Utils"
+
+function OpenCharacterWindow() return OpenMainCommandWindow("Character") end
+
+function EquipRecommendedGear()
+  if OpenCharacterWindow() then
+    Callback("Character", true, 12)
+    if AwaitAddonReady("RecommendEquip", 2) then
+      repeat
+        Callback("RecommendEquip", true, 0)
+      until AwaitAddonGone("RecommendEquip", 1)
+      repeat
+        Callback("Character", true, -1)
+      until AwaitAddonGone("Character", 1)
+      return true
+    end
+  end
+  return false
+end
