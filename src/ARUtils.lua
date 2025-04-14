2df8def2c31d5c1fa181c6207e64ff082a01d666
ShamelessEnd <shamelessend@hotmail.com>
2025-04-15 01:38:45 +1000

    ARFindCid

--- src/ARUtils.lua
+++ src/ARUtils.lua
@@ -34,6 +34,21 @@ function ARRelogTo(cid, timeout)
   return false
 end
 
+function ARFindCid(name)
+  local chars = ARGetCharacterCIDs()
+  for i = 0, chars.Count - 1 do
+    local cid = chars[i]
+    local ar_data = GetARCharacterData(cid)
+    if ar_data then
+      local cid_name = ar_data.Name.."@"..ar_data.World
+      if StringStartsWith(cid_name, name) then
+        return cid
+      end
+    end
+  end
+  return nil
+end
+
 function ARFindFishCharacterToLevel(level)
   local function hasFishingRetainer(retainer_data)
     if not retainer_data then return false end
