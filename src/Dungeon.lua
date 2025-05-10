1fdf0e4c0c33849ef1d5c6a96404a7531b0dd1f4
ShamelessEnd <shamelessend@hotmail.com>
2025-05-11 04:25:35 +1000

    add cap option to dungeon

--- src/Dungeon.lua
+++ src/Dungeon.lua
@@ -5,7 +5,7 @@ require "UINav"
 
 function OpenCurrencyWindow() return OpenMainCommandWindow("Currency") end
 
-function GetWeeklyTomeCount()
+function GetWeeklyTomeCount(max_cap)
     if not OpenCurrencyWindow() then
         return nil, nil
     end
@@ -29,17 +29,18 @@ function GetWeeklyTomeCount()
     local current_tomes = tonumber(string.sub(currency_text, 1, slash_index - 1)) or 0
     local cap_tomes = tonumber(string.sub(currency_text, slash_index + 1, string.len(currency_text))) or 0
     Logging.Debug(currency_text..": "..current_tomes.."/"..cap_tomes)
+    if max_cap and max_cap < cap_tomes then cap_tomes = max_cap end
 
     CloseAddonFast("Currency")
     return current_tomes, cap_tomes
 end
 
-function RunDutyUntilCap(duty)
-    local current_tomes, cap_tomes = GetWeeklyTomeCount()
+function RunDutyUntilCap(duty, cap)
+    local current_tomes, cap_tomes = GetWeeklyTomeCount(cap)
     while current_tomes < cap_tomes do
         ADRun(duty, 1)
         WaitUntil(ADIsStopped, nil, 1)
-        current_tomes, cap_tomes = GetWeeklyTomeCount()
+        current_tomes, cap_tomes = GetWeeklyTomeCount(cap)
     end
 end
 
@@ -47,8 +48,10 @@ function CapCharacters(character_table)
     yield("/xlenablecollection Questionable")
     yield("/wait 4")
     for _, char in pairs(character_table) do
-        if ARRelogTo(char.id) then
-            RunDutyUntilCap(char.duty)
+        if char.duty and (not char.cap or char.cap > 0) then
+            if ARRelogTo(char.id) then
+                RunDutyUntilCap(char.duty, char.cap)
+            end
         end
     end
     yield("/xldisablecollection Questionable")
