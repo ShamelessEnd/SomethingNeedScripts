5700963c469fa7bfadff9b7d56d4b27f055ba788
Joao Lippi <joao.p.lippi@gmail.com>
2025-12-19 22:07:15 -0300

    Adding weekly total cap (hardcap) check for dungeon cap

--- src/Dungeon.lua
+++ src/Dungeon.lua
@@ -6,8 +6,41 @@ require "UINav"
 function OpenCurrencyWindow() return OpenCommandWindow("currency", "Currency") end
 
 function GetWeeklyTomeCount(max_cap)
+
+    local function getTomeCount(get_total)
+
+        local function toNumberStripComma(str)
+            local comma_index, _ = string.find(str, ",")
+            if not comma_index or comma_index <= 1 then
+                return tonumber(str) or 0
+            end
+            a, b = str:match("([%d]+),([%d]+)")
+            return tonumber(table.concat({ a, b })) or 0
+        end
+
+
+        local node = 6
+        if get_total then
+            node = 5
+        end
+
+        local currency_text = GetNewNodeText("Currency", 1, 16, 200408, node)
+        while StringIsEmpty(currency_text) do
+            yield("/wait 0.1")
+            currency_text = GetNewNodeText("Currency", 1, 16, 200408, node)
+        end
+
+        local current_tomes, cap_tomes = currency_text:match("([%d,]+)/([%d,]+)")
+        current_tomes = toNumberStripComma(current_tomes)
+        cap_tomes = toNumberStripComma(cap_tomes)
+
+
+        Logging.Debug(currency_text..": "..current_tomes.."/"..cap_tomes)
+        return current_tomes, cap_tomes
+    end
+
     if not OpenCurrencyWindow() then
-        return nil, nil
+        return nil, nil, nil, nil
     end
 
     while not IsNodeVisible("Currency", 1, 16, 200408) do
@@ -15,24 +48,20 @@ function GetWeeklyTomeCount(max_cap)
         yield("/wait 0.1")
     end
 
-    local currency_text = GetNewNodeText("Currency", 1, 16, 200408, 6)
-    while StringIsEmpty(currency_text) do
-        yield("/wait 0.1")
-        currency_text = GetNewNodeText("Currency", 1, 16, 200408, 6)
+    current_weekly_tomes, cap_weekly_tomes = getTomeCount(false)
+    if not current_weekly_tomes or not cap_weekly_tomes then
+        return nil, nil, nil, nil
     end
 
-    local slash_index, _ = string.find(currency_text, "/")
-    if not slash_index or slash_index <= 1 then
-        return nil, nil
-    end
+    if max_cap and max_cap < cap_weekly_tomes then cap_weekly_tomes = max_cap end
 
-    local current_tomes = tonumber(string.sub(currency_text, 1, slash_index - 1)) or 0
-    local cap_tomes = tonumber(string.sub(currency_text, slash_index + 1, string.len(currency_text))) or 0
-    Logging.Debug(currency_text..": "..current_tomes.."/"..cap_tomes)
-    if max_cap and max_cap < cap_tomes then cap_tomes = max_cap end
+    current_total_tomes, cap_total_tomes = getTomeCount(true)
+    if not current_total_tomes or not cap_total_tomes then
+        return current_weekly_tomes, cap_weekly_tomes, nil, nil
+    end
 
     CloseAddonFast("Currency")
-    return current_tomes, cap_tomes
+    return current_weekly_tomes, cap_weekly_tomes, current_total_tomes, cap_total_tomes
 end
 
 function EnableActionStance(action, status)
@@ -64,12 +93,12 @@ function PreDutyRunChecks()
 end
 
 function RunDutyUntilCap(duty, cap)
-    local current_tomes, cap_tomes = GetWeeklyTomeCount(cap)
-    while current_tomes < cap_tomes do
+    local current_weekly_tomes, cap_weekly_tomes, current_total_tomes, cap_total_tomes = GetWeeklyTomeCount(cap)
+    while current_weekly_tomes < cap_weekly_tomes and current_total_tomes < cap_total_tomes do
         PreDutyRunChecks()
         ADRun(duty, 1)
         WaitUntil(ADIsStopped, nil, 1)
-        current_tomes, cap_tomes = GetWeeklyTomeCount(cap)
+        current_weekly_tomes, cap_weekly_tomes, current_total_tomes, cap_total_tomes = GetWeeklyTomeCount(cap)
     end
 end
 
